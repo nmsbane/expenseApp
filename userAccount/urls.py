5ddf68f2eb4bffe354e004a36c2a351e21156b8f
Madhusudan <madhusudhan638@gmail.com>
2016-05-16 15:22:45 +0000

    now the authentication is working perfectly

--- userAccount/urls.py
+++ userAccount/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     # url(r'^login/$', views.user_login, name='login')    
-    
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    #(r'^accounts/login/$', custom_login),
+    url(r'^login/$', views.custom_login, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^$', views.dashboard, name='dashboard'),
