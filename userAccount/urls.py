c992fd57866e5a1bdd2115650f938461205320df
Madhusudan <madhusudhan638@gmail.com>
2016-05-16 15:14:09 +0000

    create form for user registration, now the users can login, logout and register into the web application

--- userAccount/urls.py
+++ userAccount/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^$', views.dashboard, name='dashboard'),
 ]
 
